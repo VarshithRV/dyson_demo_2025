083c52a97fdeddfc6d714abcbcdc039910f78b49
Varshith R V <vrv278@gmail.com>
2025-11-18 17:54:54 +0800

    nothingburger

--- dual_arm_workcell_moveit_config/launch/move_group.launch.py
+++ dual_arm_workcell_moveit_config/launch/move_group.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_move_group_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("left_ur16e", package_name="dual_arm_workcell_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("dual_arm_workcell", package_name="dual_arm_workcell_moveit_config").to_moveit_configs()
     return generate_move_group_launch(moveit_config)
