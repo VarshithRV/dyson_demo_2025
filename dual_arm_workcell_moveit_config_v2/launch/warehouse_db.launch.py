4a7dc6d28ac6ae05d5fa669eb4e551cb41418400
Varshith R V <vrv278@gmail.com>
2025-11-21 03:54:07 +0800

    nothingburger

--- dual_arm_workcell_moveit_config_v2/launch/warehouse_db.launch.py
+++
@@ -1,7 +0,0 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_warehouse_db_launch
-
-
-def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("dual_arm_workcell", package_name="dual_arm_workcell_moveit_config_v2").to_moveit_configs()
-    return generate_warehouse_db_launch(moveit_config)
