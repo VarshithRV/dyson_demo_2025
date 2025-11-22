47faa592eab94f66595f2db7324417c367c305e7
Varshith R V <vrv278@gmail.com>
2025-11-22 22:25:44 +0800

    left camera changed to 415 and calibrated

--- dual_arm_workcell_bringup/launch/dual_arm_workcell_bringup.launch.py
+++ dual_arm_workcell_bringup/launch/dual_arm_workcell_bringup.launch.py
@@ -42,7 +42,7 @@ def launch_setup():
         launch_arguments={
             'camera_name': 'left_camera',
             'align_depth.enable': 'true',
-            'serial_no': '_135122075246',
+            'serial_no': '_210622060509',
             'pointcloud.enable': 'true',
             'spatial_filter.enable': 'true',
             'temporal_filter.enable': 'true',
